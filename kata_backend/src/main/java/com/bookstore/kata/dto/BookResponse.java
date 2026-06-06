102a872839b1c57c119537c8b84c313d04b85702
Kishoreraj Jayakumar <30684694+kishore-raj@users.noreply.github.com>
2026-06-06 22:18:33 +0530

    Removing my notes
    Removed notes &  comments

--- kata_backend/src/main/java/com/bookstore/kata/dto/BookResponse.java
+++ kata_backend/src/main/java/com/bookstore/kata/dto/BookResponse.java
@@ -1,11 +1,6 @@
 package com.bookstore.kata.dto;
 
-// In Java, a record provides several options and features out-of-the-box:
-// 1. Compact declaration of an immutable data class.
-// 2. Automatically provides final fields, a canonical constructor, getters (named as field()), equals(), hashCode(), and toString().
-// 3. You can declare additional methods, static fields, static methods, and compact constructors inside the record.
 
-// Example showing available options in a record:
 public record BookResponse(
     Long id,
     String title,
